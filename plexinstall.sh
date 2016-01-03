fa3d72ef6ddf500cd7824e444a4e26413aec098e
gyula72 <gyula72@users.noreply.github.com>
2016-01-03 17:45:39 +0100

    Update plexinstall.sh

--- plexinstall.sh
+++ plexinstall.sh
@@ -2,4 +2,15 @@ sudo mkdir /tmp
 cd /tmp
 sudo wget https://dev2day.de/pms/pool/main/p/plexmediaserver/plexmediaserver_0.9.14.6.1620-e0b7243-1~jessie_armhf.deb
 sudo dpkg -i plex*
+sudo rm plex*
 sudo service plexmediaserver restart
+
+sudo mkdir /Desktop
+cd /Desktop
+sudo rm *  
+sudo openssl req -new -nodes -newkey rsa:2048 -out /Desktop/iMovie.pem -keyout /Desktop/iMovie.key -x509 -days 7300 -subj "/C=US/CN=www.icloud.com"
+
+sudo openssl x509 -in /Desktop/iMovie.pem -outform der -out /Desktop/iMovie.cer && cat /Desktop/iMovie.key >> /Desktop/iMovie.pem
+
+sudo mv /Desktop/iMovie.cer /usr/local/lib/PlexConnect/PlexConnect-master/assets/certificates/iMovie.cer
+sudo mv /Desktop/iMovie.pem /usr/local/lib/PlexConnect/PlexConnect-master/assets/certificates/iMovie.pem
