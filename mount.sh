cf63cc34206c1790f3b953dbff6f852ab0129f73
gyula72 <gyula72@users.noreply.github.com>
2017-01-02 12:38:57 +0100

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get install ntfs-3g
-sudo echo '/dev/sda1 /mnt ntfs defaults,umask=000' >> /etc/fstab
+sudo echo '/dev/sda1 /mnt ntfs defaults,umask=000' >> //etc/fstab
 sudo mount /mnt
